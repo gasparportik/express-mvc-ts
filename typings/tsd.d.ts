df3d1d4462cb14e7755b4deebbd352a3c36c4fca
boykathemad <madboyka@yahoo.com>
2016-10-09 17:05:32 +0300

    Removed typing files, will use npm install @types/*

--- typings/tsd.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="es6.d.ts" />
-/// <reference path="express/express.d.ts" />
-/// <reference path="mime/mime.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="serve-static/serve-static.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="reflect-metadata/reflect-metadata.d.ts" />
